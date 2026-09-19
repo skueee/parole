cdf399aeb4aa68a223ef25874d7e1d511f8a2f0a
skueee <skueee@outlook.com>
2026-09-19 12:37:53 +0200

    Ran lint and format

--- src/parole/main.py
+++ src/parole/main.py
@@ -1,17 +1,39 @@
+import subprocess
+
 from textual.app import App, ComposeResult
 from textual.widgets import Label
-import subprocess
 
 
 def get_metadata():
-    run = subprocess.run(["playerctl", "status"], capture_output=True, text=True).returncode == 0
+    run = (
+        subprocess.run(
+            ["playerctl", "status"], capture_output=True, text=True, check=False
+        ).returncode
+        == 0
+    )
     if run:
-        artist = subprocess.run(["playerctl", "metadata", "artist"], capture_output=True, text=True).stdout.strip()
-        title = subprocess.run(["playerctl", "metadata", "title"], capture_output=True, text=True).stdout.strip()
-        album = subprocess.run(["playerctl", "metadata", "album"], capture_output=True, text=True).stdout.strip()
+        artist = subprocess.run(
+            ["playerctl", "metadata", "artist"],
+            capture_output=True,
+            text=True,
+            check=False,
+        ).stdout.strip()
+        title = subprocess.run(
+            ["playerctl", "metadata", "title"],
+            capture_output=True,
+            text=True,
+            check=False,
+        ).stdout.strip()
+        album = subprocess.run(
+            ["playerctl", "metadata", "album"],
+            capture_output=True,
+            text=True,
+            check=False,
+        ).stdout.strip()
         return [run, artist, title, album]
     else:
         return [run]
+
 
 class MyApp(App):
     CSS_PATH = "textual.tcss"
